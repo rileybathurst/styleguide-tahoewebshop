ce4c3f0f7ab6443d0a81b6338884ca896a8ae28c
Riley <riley@rileybathurst.com>
2022-04-12 20:03:19 -0700

    storybook

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-addon-gatsby",
+    '@storybook/preset-scss',
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+# Tahoe Web Shop
+
+## Style Guide
+
 https://styleguidetahoewebshopmain.gatsbyjs.io/
+
+I'm going to build another version of the storybook but that can't be on gatsby cloud as thats for gatsby build 🙄
