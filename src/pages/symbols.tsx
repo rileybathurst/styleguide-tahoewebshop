affd9740dd15390e45e009fae5316ad6a680d487
Riley <riley@rileybathurst.com>
2022-07-28 11:38:23 -0700

    symbols

--- src/pages/symbols.tsx
+++ src/pages/symbols.tsx
@@ -7,8 +7,17 @@ const SymbolsPage = () => {
       <title>Symbols</title>
       <h1>Symbols</h1>
       <p>
+        <a
+          href="https://www.w3.org/wiki/Common_HTML_entities_used_for_typography"
+          target="blank"
+          rel="noopener noreffer"
+        >W3C Common HTML entities used for typography
+        </a>
+        <ul>
+          <li><code>&thinsp; & thinsp; - thin space</code></li>
+        </ul>
+        backup <br />
         https://www.toptal.com/designers/htmlarrows/
-      // TODO add some of my commonly used here
       </p>
       <Link to="/">Go home</Link>.
     </main>
