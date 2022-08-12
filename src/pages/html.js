85a18bf999cc68dfefe5c85a6ca2ce3ca133a6f7
Riley <riley@rileybathurst.com>
2022-08-11 18:13:02 -0700

    color needs to be a page

---
+++ src/pages/html.js
@@ -0,0 +1,24 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const LinksPage = () => {
+  return (
+    <main>
+      <title>HTML</title>
+      <h1>HTML</h1>
+      <p>
+        Layout a file in these orders
+
+        <ol>
+          <li>Header or layout</li>
+          <li>Seo</li>
+          <li>Breadcrumbs</li>
+          <li>Content</li>
+          <li>Footer</li>
+        </ol>
+      </p>
+    </main>
+  )
+}
+
+export default LinksPage
