72134b8c34d57072b45231a2c286bfe5523e1a94
Riley <riley@rileybathurst.com>
2022-10-26 15:46:17 -0700

    seo ideas

--- src/pages/seo.js
+++ src/pages/seo.js
@@ -5,9 +5,53 @@ const SeoPage = () => {
     <main>
       <title>SEO</title>
       <h1>SEO</h1>
+
+      <p>As much as sticking to Schema.org is great the first step is to work with everything functioning on google first</p>
+
+      <p>EBPT is on the new Gatsby Head API showcasing how that works and the use of JSON LD to get around problems with html attributes</p>
+
       <p>
-        documentation is coming
+        these are the ones google uses
+        // TODO cross reference these with the google developer docs
+        https://developers.google.com/search/docs/appearance/structured-data/search-gallery
+
+        <ul>
+          <li>Article</li>
+          <li>Book</li>
+          <li>Breadcrumb</li>
+          <li>Carousel</li>
+          <li>Course</li>
+          <li>COVID-19 announcements</li>
+          <li>Dataset</li>
+          <li>Education Q&A</li>
+          <li>Employer Aggregate Rating</li>
+          <li>Estimated salary</li>
+          <li>Event</li>
+          <li>Fact Check</li>
+          <li>FAQ</li>
+          <li>Home Activities</li>
+          <li>How-to</li>
+          <li>Image metadata</li>
+          <li>Job Posting</li>
+          <li>Learning Video</li>
+          <li>Local Business</li>
+          <li>Logo</li>
+          <li>Math solver</li>
+          <li>Movie</li>
+          <li>Practice problem</li>
+          <li>Product</li>
+          <li>Q&A</li>
+          <li>Recipe</li>
+          <li>Review snippet</li>
+          <li>Sitelinks search box</li>
+          <li>Software App</li>
+          <li>Speakable</li>
+          <li>Subscription and paywalled content</li>
+          <li>Video</li>
+        </ul>
       </p>
+
+
     </main>
   )
 }
